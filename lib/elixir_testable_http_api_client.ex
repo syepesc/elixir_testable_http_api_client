cc527d5e896fea32a9b82923fabd0f9c1d979f80
Santiago Yepes <syepesc@hotmail.com>
2024-02-12 10:36:45 -0500

    Initial commit

---
+++ lib/elixir_testable_http_api_client.ex
@@ -0,0 +1,18 @@
+defmodule ElixirTestableHttpApiClient do
+  @moduledoc """
+  Documentation for `ElixirTestableHttpApiClient`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirTestableHttpApiClient.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
